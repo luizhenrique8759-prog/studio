74c69f64cb2f65b5cfc8ba54b0a4b4ae1186706e
Luiz Henriqu Oliveira Araujo <luizhenrique8759@gmail.com>
2026-04-19 03:01:00 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyDgh-cmYCukPLjG8eH74ACjKbwnYD-Bri0",
-  authDomain: "agenda-9a793.firebaseapp.com",
-  projectId: "agenda-9a793",
-  storageBucket: "agenda-9a793.firebasestorage.app",
-  messagingSenderId: "10184242249",
-  appId: "1:10184242249:web:1b7660a8e1f89054bd7b98",
-  measurementId: "G-N806492ZQ9"
+  "projectId": "agenda-9a793",
+  "appId": "1:10184242249:web:078668d4f475f340bd7b98",
+  "apiKey": "AIzaSyDgh-cmYCukPLjG8eH74ACjKbwnYD-Bri0",
+  "authDomain": "agenda-9a793.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "10184242249"
 };
